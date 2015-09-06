e50dfad8e7fa8c87907c94e154e5130a6c1cf792
ganchau <gancchau@gmail.com>
2015-09-06 18:32:05 -0400

    added personal venue photos into second tab view controller

---
+++ TheDish/TheDish/PersonalVenueTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PersonalVenueTableViewCell.m
+//  TheDish
+//
+//  Created by Gan Chau on 9/6/15.
+//  Copyright (c) 2015 GC App. All rights reserved.
+//
+
+#import "PersonalVenueTableViewCell.h"
+
+@implementation PersonalVenueTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
